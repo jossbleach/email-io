9b3f0739443f089f9fa77247882997103c9c147a
Joss Bleach <jossjbleach@gmail.com>
2018-09-04 22:09:58 +0100

    cookies added

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => console.log(err));
+});
+
 passport.use(
   new GoogleStrategy(
     {
