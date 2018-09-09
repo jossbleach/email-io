cea498b35914a7149148b847448d16cdeb6d7181
Joss Bleach <jossjbleach@gmail.com>
2018-09-09 11:52:19 +0100

    back to master

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "316295698677-b081gf9bcoqmj81al6qo8dt1i02m4pv3.apps.googleusercontent.com",
-  googleClientSecret: "WdRDcQoegenAyHJaUZVQuno9",
-  mongoURI:
-    "mongodb://jossbleach:password123@ds243812.mlab.com:43812/email-io-dev",
-  cookieKey: "GPIthEF4q24SEu7J0wqJ0rJPfEHm88ov3VKOVLOr"
-};
